1b0d2d1b5d9ed1ea754dccb4aa7552ac0567f641
Himanshi <himanshiagarwal001@gmail.com>
2026-05-22 22:31:14 +0530

    Data types in java

---
+++ src/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes {
+    public static void main (String args[]){
+        String employeeName = "Rakesh Sharma";
+        boolean isTerminated = false;
+        int employeeId = 45;
+        double employeeSalary= 23000.45d;
+        char employeeShortCode= 'R';
+        System.out.println("The employee details are as follows : ");
+        System.out.println("Employee Id : "+ employeeId + ", " + employeeName);
+        System.out.println("Is employee terminated : "+ isTerminated);
+        System.out.println(employeeShortCode +": "+ employeeSalary);
+    }
+}
