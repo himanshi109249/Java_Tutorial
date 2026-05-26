7acc514312b020c3f7a364cd310e5ef5fc27c80c
Himanshi <himanshiagarwal001@gmail.com>
2026-05-26 22:18:43 +0530

    Math Class in java

---
+++ src/MathClass.java
@@ -0,0 +1,17 @@
+
+public class MathClass {
+    public static void main (String args[]){
+        System.out.println(Math.max(4,7));
+        System.out.println(Math.min(4,7));
+        System.out.println(Math.sqrt(4));
+        System.out.println(Math.abs(-4.5));
+        System.out.println(Math.pow(2,3));
+
+        System.out.println(Math.round(4.5));
+        System.out.println(Math.ceil(4.2));
+        System.out.println(Math.floor(4.7));
+
+        System.out.println((int) (Math.random() * 10));
+
+    }
+}
