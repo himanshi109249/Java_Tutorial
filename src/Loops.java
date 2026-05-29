2077a5aa85f405756c3235ec2fc2d9a03f81b85a
Himanshi <himanshiagarwal001@gmail.com>
2026-05-29 22:51:04 +0530

    Loops in java

---
+++ src/Loops.java
@@ -0,0 +1,18 @@
+public class Loops {
+    public static void main (String[] args){
+        int n=3;
+//        for(int i=0; i<=n; i++){
+//            System.out.println("*");
+//        }
+
+        while(n<=8){
+            System.out.println(n);
+            n++;
+        }
+        int i=1;
+        do{
+            System.out.println("*");
+            i++;
+        }while(i<=5);
+    }
+}
